0270729b25119420965ad7043a457d2499f8d5a4
johnstoc13 <66090689+johnstoc13@users.noreply.github.com>
2020-08-02 19:18:14 -0400

    validated, removed console, temp literal

--- assets/script.js
+++ assets/script.js
@@ -5,14 +5,15 @@ $(document).ready(function () {
     let city;
     let lat;
     let lon;
+    let cityArray;
 
     // Put current day, month, and date onto page & 5 day forecast dates
-    $("#date").append(moment().format("MM-DD-YYYY"));
-    $("#date1").append(moment().add(1, 'd').format("MM-DD-YYYY"));
-    $("#date2").append(moment().add(2, 'd').format("MM-DD-YYYY"));
-    $("#date3").append(moment().add(3, 'd').format("MM-DD-YYYY"));
-    $("#date4").append(moment().add(4, 'd').format("MM-DD-YYYY"));
-    $("#date5").append(moment().add(5, 'd').format("MM-DD-YYYY"));
+    $(".date").append(moment().format("MM-DD-YYYY"));
+    $(".date1").append(moment().add(1, 'd').format("MM-DD-YYYY"));
+    $(".date2").append(moment().add(2, 'd').format("MM-DD-YYYY"));
+    $(".date3").append(moment().add(3, 'd').format("MM-DD-YYYY"));
+    $(".date4").append(moment().add(4, 'd').format("MM-DD-YYYY"));
+    $(".date5").append(moment().add(5, 'd').format("MM-DD-YYYY"));
 
     // Function to run when a search is executed
     $("#searchBtn").on("click", function () {
@@ -21,16 +22,15 @@ $(document).ready(function () {
         // Run necessary functions
         storeSearchedCities();
         loadSearchHistory();
+        // Clear search field
         $(".searchField").val("");
-        // localStorage.setItem("savedCity", JSON.stringify());
     });
 
     // Get current weather
     function getCurrentWeather(currentCity) {
         // Get city for the queryURL
-        
         city = currentCity || $("input").val();
-        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=7de3b9a0cd9f9144a9dcc3a15797e2b5";
+        let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=7de3b9a0cd9f9144a9dcc3a15797e2b5`;
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -39,7 +39,7 @@ $(document).ready(function () {
             lat = response.coord.lat;
             lon = response.coord.lon;
             // Label city on page
-            $("#city").text(response.name + "\xa0\xa0");
+            $(".city").text(response.name + "\xa0\xa0");
             getFiveDayForecast();
         });
     }
@@ -47,40 +47,38 @@ $(document).ready(function () {
     // Get future forecast information
     function getFiveDayForecast() {
         $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&units=imperial&appid=7de3b9a0cd9f9144a9dcc3a15797e2b5",
+            url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&units=imperial&appid=7de3b9a0cd9f9144a9dcc3a15797e2b5`,
             method: "GET"
         }).then(function (response) {
             // Set current weather stats and icon
-            $("#wxIcon").attr("src", "https://openweathermap.org/img/wn/" + response.current.weather[0].icon + "@2x.png");
-            $("#temp").text(Math.round(response.current.temp));
-            $("#humidity").text(response.current.humidity);
-            $("#wind").text(Math.round(response.current.wind_speed));
-            $("#uvindex").text(response.current.uvi);
+            $(".wxIcon").attr("src", `https://openweathermap.org/img/wn/${response.current.weather[0].icon}@2x.png`);
+            // Cited:  https://www.w3schools.com/jsref/jsref_round.asp
+            $(".temp").text(Math.round(response.current.temp));
+            $(".humidity").text(response.current.humidity);
+            $(".wind").text(Math.round(response.current.wind_speed));
+            $(".uvindex").text(response.current.uvi);
             // Loop through 5 days to get forecast and display on page
             forecast.each(function (day) {
                 day++;
                 const _this = $(this);
-                _this.find("#futureIcon").attr("src", "https://openweathermap.org/img/wn/" + response.daily[day].weather[0].icon + "@2x.png");
-                _this.find("#futureTemp").text(Math.round(response.daily[day].temp.day));
-                _this.find("#futureHumidity").text(response.daily[day].humidity);
+                _this.find(".futureIcon").attr("src", `https://openweathermap.org/img/wn/${response.daily[day].weather[0].icon}@2x.png`);
+                _this.find(".futureTemp").text(Math.round(response.daily[day].temp.day));
+                _this.find(".futureHumidity").text(response.daily[day].humidity);
             });
             // Set uvindex color based on condition
             if (response.current.uvi < 2) {
-                $("#uvindex").attr("class", "bg-success text-white rounded px-1");
+                $(".uvindex").attr("class", "bg-success text-white rounded px-1");
             } else if (response.current.uvi >= 2 && response.current.uvi < 6) {
-                $("#uvindex").attr("class", "bg-warning text-body rounded px-1");
+                $(".uvindex").attr("class", "bg-warning text-body rounded px-1");
             } else if (response.current.uvi >= 6 && response.current.uvi < 8) {
-                $("#uvindex").attr("class", "bg-orange text-dark rounded px-1");
+                $(".uvindex").attr("class", "bg-orange text-dark rounded px-1");
             } else if (response.current.uvi >= 8 && response.current.uvi < 11) {
-                $("#uvindex").attr("class", "bg-danger text-white rounded px-1");
+                $(".uvindex").attr("class", "bg-danger text-white rounded px-1");
             } else {
-                $("#uvindex").attr("class", "bg-purple text-white rounded px-1");
+                $(".uvindex").attr("class", "bg-purple text-white rounded px-1");
             }
         });
     }
-
-    // Define empty array
-    let cityArray;
 
     // Get stored cities
     function loadSearchHistory() {
@@ -90,10 +88,10 @@ $(document).ready(function () {
             cityArray = [];
             storedCities = cityArray;
             localStorage.setItem("storedCities", JSON.stringify(cityArray));
-            // Otherwise delcare
+            // Otherwise get last city searched weather displayed
         } else {
             cityArray = storedCities;
-            let lastCitySearched = cityArray[storedCities.length - 1]
+            let lastCitySearched = cityArray[storedCities.length - 1];
             getCurrentWeather(lastCitySearched);
         }
         displayHistory();
@@ -104,19 +102,15 @@ $(document).ready(function () {
         cityArray.push(city);
         localStorage.setItem("storedCities", JSON.stringify(cityArray));
     }
-    
+
     // Display search history in side bar
     function displayHistory() {
         const allCities = $("div.searchedCities");
         let storedCities = JSON.parse(localStorage.getItem("storedCities"));
-        // console.log(storedCities);
         allCities.empty();
-        // let idCounter = 0
         $.each(storedCities, function (index) {
             const newCityButton = $("<button>").addClass("history btn btn-block bg-white text-secondary text-left border-secondary rounded-0 py-2 mt-0").attr("id", storedCities[index]);
             allCities.prepend(newCityButton.text(storedCities[index]));
-            // idCounter++;
-            // console.log(storedCities[index]);
         });
     }
 
@@ -125,39 +119,13 @@ $(document).ready(function () {
     $(document).on("click", "button.history", function (event) {
         event.preventDefault();
         pastCity = event.target;
-        console.log(pastCity);
         pastCity = pastCity.getAttribute("id");
-        console.log(pastCity);
         getCurrentWeather(pastCity);
-        // let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + pastCity + "&units=imperial&appid=7de3b9a0cd9f9144a9dcc3a15797e2b5";
-        // $.ajax({
-        //     url: queryURL,
-        //     method: "GET"
-        // }).then(function (response) {
-        //     // Get lat/lon for onecall to retrieve 5 day forecast info
-        //     lat = response.coord.lat;
-        //     lon = response.coord.lon;
-        //     // Label city on page
-        //     $("#city").text(response.name + "\xa0\xa0");
-        //     getFiveDayForecast();
-        // });
     });
 
     // Load search history on refresh of page
     loadSearchHistory();
 
-
-
-
-
-    // *****************************           WHAT'S LEFT...             *********************************
-    // 
-    // 
-    // 
-    // 
     // Weather to load "some city" on initial load if nothing stored.
-    // APPLICATION LOADS LAST SEARCHED CITY FORECAST ON PAGE LOAD
-    // Past cities need to reveal weather when clicked.....
-    // Responsiveness needs some work
-    
+
 });
